1e75d10ecf51fe3b1c0768a934d90f253e87a14d
Supercoder2311 <84426763+Supercoder2311@users.noreply.github.com>
2022-03-30 19:50:42 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // https://teachablemachine.withgoogle.com/models/EZVHEj4Dg/
+
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifer = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/EZVHEj4Dg/model.json", modelReady);
+}
+function modelReady(){
+    classifer.classify(gotResults);
+}
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
